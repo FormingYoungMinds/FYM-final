f43da45c21cafe1e3e357eaa268f6a2fedcef8fe
Yizo-Yizo <56066066+Yizo-Yizo@users.noreply.github.com>
2020-03-13 12:52:07 +0200

    RestClient

--- FYMs/FYMs/Models/User.cs
+++ FYMs/FYMs/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,18 @@ namespace FYMs.Models
 {
     class User
     {
+        [PrimaryKey, AutoIncrement]
+
+        public int ID { get; set; }
+
+        public string FirstNames { get; set; }
+
+        public string LastName { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
     }
 }
