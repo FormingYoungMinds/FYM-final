9976e6651dc4b477bb0f80693664d5893bd21b77
Yizo-Yizo <selebedi.thapelo6@gmail.com>
2020-04-03 17:30:03 +0200

    WebApi

---
+++ FYMAppSln/FYMWebAPI/Models/FYMAppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYMWebAPI.Models
+{
+    public class FYMAppContext : DbContext
+    {
+        public FYMAppContext(DbContextOptions<FYMAppContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ FYMAppSln/FYMWebAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYMWebAPI.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        public int Cellnumber { get; set; }
+
+        public string Password { get; set; }
+    }
+}
